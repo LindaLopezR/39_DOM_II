04af05751942c9d77346aaf44122976e84c05e24
LindaLopezR <guadalupelopez.lr@gmail.com>
2016-10-18 16:50:06 +0000

    Bicis locas parte 2

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,6 +1,24 @@
 function validateForm(){
 	var nombre = document.getElementById('name').value;
-	if(nombre === ""){
-		alert('Todos los campos son requeridos');
+	var apellido = document.getElementById('lastname').value;
+	var correo = document.getElementById('input-email').value;
+	var password = document.getElementById('input-password').value;
+	var indice = document.getElementById("opciones").selectedIndex;
+	letras = /^[A-Z][a-zA-Z]*$/;
+	expresion = /\w+@+\w+\.+[a-z]/;
+	if(nombre === "" || apellido === "" || correo === "" || password === ""){
+		document.getElementById('alertaGeneral').style.display = 'inline';
+	} else if(!letras.test(nombre)){
+		return document.getElementById('alertaNombre').style.display = 'inline';
+	} else if(!letras.test(apellido)){
+		return document.getElementById('alertaApellido').style.display = 'inline';
+	} else if(!expresion.test(correo)){
+		return document.getElementById('alertaCorreo').style.display = 'inline';
+	} else if(password.length<6){
+		return document.getElementById('alertaPasswordI').style.display = 'inline';
+	} else if(password==="password" || password==="123456" || password==="098754"){
+		return document.getElementById('alertaPasswordII').style.display = 'inline';
+	} else if(indice == 0 ) {
+  		return document.getElementById('alertaBici').style.display = 'inline';
 	}
 }
